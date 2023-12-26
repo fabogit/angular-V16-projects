cf903756ca7027a7e658ce33c0cdb8b539abd561
fabo <fabo.dev.email@gmail.com>
2023-12-26 21:41:28 +0100

    #5
    articles module and routing

--- 02_spa-blog/src/app/app-routing.module.ts
+++ 02_spa-blog/src/app/app-routing.module.ts
@@ -3,7 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  {path: 'contact', component: ContactComponent}
+  { path: 'contact', component: ContactComponent },
+  // Lazy loading articles.
+  // It creates a separate bundle for each lazy-loaded module, which is loaded upon request,
+  //  reducing the final bundle size and the memory consumption of the application
+  { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) },
+  // redirect
+  { path: '', pathMatch: 'full', redirectTo: 'articles' },
+  // wildcard route
+  { path: '**', redirectTo: 'articles' }
 ];
 
 @NgModule({
