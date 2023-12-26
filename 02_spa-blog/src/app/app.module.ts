a4012ba5420390dd868bbb9239e2124281e42ece
fabo <fabo.dev.email@gmail.com>
2023-12-26 21:25:44 +0100

    #4
    contact module and routing

--- 02_spa-blog/src/app/app.module.ts
+++ 02_spa-blog/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { ContactModule } from './contact/contact.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    ContactModule
   ],
   providers: [],
   bootstrap: [AppComponent]
