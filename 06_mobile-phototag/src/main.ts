732fdea1eeb4d8c557dfe08b4da5e1c9a347dc3e
fabo <fabo.dev.email@gmail.com>
2023-12-29 17:49:13 +0100

    #39
    Previewing photos with CesiumJS - Configuring CesiumJS

--- 06_mobile-phototag/src/main.ts
+++ 06_mobile-phototag/src/main.ts
@@ -6,7 +6,6 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 
-
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
@@ -15,13 +14,15 @@ if (environment.production) {
   enableProdMode();
 }
 
+(window as Record<string, any>)['CESIUM_BASE_URL'] = '/assets/cesium/';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBIJe-YfnvUTexNQwjbLhsfHkaS239TeSs",
-  authDomain: "phototag-8f01d.firebaseapp.com",
-  projectId: "phototag-8f01d",
-  storageBucket: "phototag-8f01d.appspot.com",
-  messagingSenderId: "961601248064",
-  appId: "1:961601248064:web:67d41deea0a2916ebc78a5"
+  apiKey: 'AIzaSyBIJe-YfnvUTexNQwjbLhsfHkaS239TeSs',
+  authDomain: 'phototag-8f01d.firebaseapp.com',
+  projectId: 'phototag-8f01d',
+  storageBucket: 'phototag-8f01d.appspot.com',
+  messagingSenderId: '961601248064',
+  appId: '1:961601248064:web:67d41deea0a2916ebc78a5',
 };
 
 bootstrapApplication(AppComponent, {
@@ -29,8 +30,10 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     importProvidersFrom(IonicModule.forRoot({})),
     provideRouter(routes),
-    importProvidersFrom(provideFirebaseApp(() => initializeApp(firebaseConfig))),
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(firebaseConfig))
+    ),
     importProvidersFrom(provideFirestore(() => getFirestore())),
-    importProvidersFrom(provideStorage(() => getStorage()))
+    importProvidersFrom(provideStorage(() => getStorage())),
   ],
 });
