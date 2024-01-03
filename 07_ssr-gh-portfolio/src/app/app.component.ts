ec462664f397bc72c29d40ace6e1f71c871cd93f
fabo <fabo.dev.email@gmail.com>
2024-01-03 03:53:43 +0100

    #48
    Enhancing SEO capabilities

--- 07_ssr-gh-portfolio/src/app/app.component.ts
+++ 07_ssr-gh-portfolio/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+
 import { GithubService } from './github.service';
 
 @Component({
@@ -10,8 +12,25 @@ export class AppComponent implements OnInit {
   title = 'gh-portfolio';
   username = '';
 
-  constructor(private githubService: GithubService) { }
+  constructor(
+    private githubService: GithubService,
+    private titleService: Title,
+    private meta: Meta
+  ) { }
+
   ngOnInit(): void {
     this.username = this.githubService.username;
+    // SEO optimization
+    this.titleService.setTitle('GitHub portfolio app');
+    this.meta.addTags([
+      {
+        name: 'description',
+        content: `${this.username}'s GitHub portfolio`,
+      },
+      {
+        name: 'author',
+        content: this.username,
+      },
+    ]);
   }
 }
